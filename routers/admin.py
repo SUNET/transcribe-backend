fe990d7354227e90739486043d97d7c3dbc6d419
Kristofer Hallin <kristofer@sunet.se>
2026-01-23 09:34:08 +0100

    Hide certain customer endpoints.

--- routers/admin.py
+++ routers/admin.py
@@ -410,7 +410,7 @@ async def group_stats(
     )
 
 
-@router.get("/admin/customers")
+@router.get("/admin/customers", include_in_schema=False)
 async def list_customers(
     request: Request,
     admin_user: dict = Depends(get_current_admin_user),
@@ -438,7 +438,7 @@ async def list_customers(
     return JSONResponse(content={"result": result})
 
 
-@router.post("/admin/customers")
+@router.post("/admin/customers", include_in_schema=False)
 async def create_customer(
     request: Request,
     item: CreateCustomerRequest,
@@ -478,7 +478,7 @@ async def create_customer(
     return JSONResponse(content={"result": customer})
 
 
-@router.get("/admin/customers/{customer_id}")
+@router.get("/admin/customers/{customer_id}", include_in_schema=False)
 async def get_customer(
     request: Request,
     customer_id: str,
@@ -502,7 +502,7 @@ async def get_customer(
     return JSONResponse(content={"result": customer})
 
 
-@router.put("/admin/customers/{customer_id}")
+@router.put("/admin/customers/{customer_id}", include_in_schema=False)
 async def update_customer(
     request: Request,
     item: UpdateCustomerRequest,
@@ -543,7 +543,7 @@ async def update_customer(
     return JSONResponse(content={"result": customer})
 
 
-@router.delete("/admin/customers/{customer_id}")
+@router.delete("/admin/customers/{customer_id}", include_in_schema=False)
 async def delete_customer(
     request: Request,
     customer_id: int,
@@ -570,7 +570,7 @@ async def delete_customer(
     return JSONResponse(content={"result": {"status": "OK"}})
 
 
-@router.get("/admin/realms")
+@router.get("/admin/realms", include_in_schema=False)
 async def list_realms(
     request: Request,
     admin_user: dict = Depends(get_current_admin_user),
@@ -592,7 +592,7 @@ async def list_realms(
     return JSONResponse(content={"result": get_all_realms()})
 
 
-@router.get("/admin/customers/{customer_id}/stats")
+@router.get("/admin/customers/{customer_id}/stats", include_in_schema=False)
 async def customer_stats(
     request: Request,
     customer_id: str,
